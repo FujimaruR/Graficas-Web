66c54afc65d303c287dbeff0e388daecb2075877
VALLOID3 <Josearsant@hotmail.com>
2024-05-24 18:06:35 -0600

    Update confidatabase.js

--- Back/confidatabase.js
+++ Back/confidatabase.js
@@ -1,4 +1,4 @@
-firebase.initializeApp({
+var firebaseConfifg = {
     apiKey: "AIzaSyCLhuEoB2Il9yUh3YIJMB3vEu0OY1H8b4I",
   authDomain: "spacegame-87bcd.firebaseapp.com",
   projectId: "spacegame-87bcd",
@@ -6,6 +6,6 @@ firebase.initializeApp({
   messagingSenderId: "561048026800",
   appId: "1:561048026800:web:ad1a10dc9de9aa75608575",
   measurementId: "G-HEE15XCNW5"
-});
+};
 
-var bd = firebase.firestore();
+firebase.initializeApp(firebaseConfig)
